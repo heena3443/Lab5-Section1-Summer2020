d033cb8cec17864533c38401ab3b6f0fbcc08255
EdwardDL97 <50053212+EdwardDL97@users.noreply.github.com>
2020-07-11 11:56:53 -0500

    add new line with print statement

--- Lab5/src/FirstCommit.java
+++ Lab5/src/FirstCommit.java
@@ -11,5 +11,6 @@ public class FirstCommit {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore and my hobby is singing.");
+		System.out.println("My name is Edward Diaz Lopez and my hobby is playing video games.");
 	}
 }
