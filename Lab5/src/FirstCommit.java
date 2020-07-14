c1177fc6eba3020f1aaa2fe38f759b8dd12c85a5
gabriella <56049684+gmariello@users.noreply.github.com>
2020-07-14 10:15:16 -0500

    Update FirstCommit.java
    Added my name to the Java File

--- Lab5/src/FirstCommit.java
+++ Lab5/src/FirstCommit.java
@@ -17,6 +17,7 @@ public class FirstCommit {
 		System.out.println("My name is Christopher Martinez and my hobby is playing games.");
 		System.out.println("My name is Luke Puente and my hobby is riding my bike.");
 		System.out.println("My name is Michelle Richardson and my hobby is exercising.");
+		System.out.println("My name is Gabriella Mariello and my hobbie is hanging with friends");
 
 	}
 }
