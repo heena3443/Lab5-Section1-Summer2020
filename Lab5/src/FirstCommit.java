43bfe99ba40b2d74f340f2ce92c8a6bf3d3c01ae
TheBisping <afkeying@gmail.com>
2020-07-14 16:31:26 -0500

    Update FirstCommit.java
    fixed typo

--- Lab5/src/FirstCommit.java
+++ Lab5/src/FirstCommit.java
@@ -21,7 +21,7 @@ public class FirstCommit {
 		System.out.println("My name is Rongrong Wei and my hobby is long distance running.");
 		System.out.println("My name is Anthony Jreij and my hobby is painting.");
 		System.out.println("My name is Johann Tschetter and my hobby is singing");
-		System.out.println("My name is Michael Bisping and my hobby is trialoths.");
+		System.out.println("My name is Michael Bisping and my hobby is triathlons.");
 
 	}
 }
