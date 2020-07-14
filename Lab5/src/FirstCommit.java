dc0e1fb3bd36d7321738e3e2f26b2aea1b419ec2
MichRich25 <67449190+MichRich25@users.noreply.github.com>
2020-07-14 07:34:21 -0500

    Update FirstCommit.java
    add new print line

--- Lab5/src/FirstCommit.java
+++ Lab5/src/FirstCommit.java
@@ -16,6 +16,7 @@ public class FirstCommit {
 		System.out.println("My name is Clinton Welding and my hobby is watching Football.");
 		System.out.println("My name is Christopher Martinez and my hobby is playing games.");
 		System.out.println("My name is Luke Puente and my hobby is riding my bike.");
+		System.out.println("My name is Michelle Richardson and my hobby is exercising.");
 
 	}
 }
