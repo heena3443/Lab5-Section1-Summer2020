d02eaa4ece0dbe621eeb57bf28a83555dce58cbf
AnthonyJreij <anthony.jreij1@gmail.com>
2020-07-14 13:53:41 -0500

    JreijFirstEdit

--- Lab5/src/FirstCommit.java
+++ Lab5/src/FirstCommit.java
@@ -18,6 +18,7 @@ public class FirstCommit {
 		System.out.println("My name is Luke Puente and my hobby is riding my bike.");
 		System.out.println("My name is Michelle Richardson and my hobby is exercising.");
 		System.out.println("My name is Rongrong Wei and my hobby is long distance running.");
+		System.out.println("My name is Anthony Jreij and my hobby is painting.");
 
 	}
 }
